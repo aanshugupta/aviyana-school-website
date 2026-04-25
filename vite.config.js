31ded613fa12ef9754baa871e03f930310beda19
aanshugupta <aanshugupta2005@gmail.com>
2026-04-25 15:42:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/aviyana-school-website/',   // ✅ ADD THIS LINE
+
   plugins: [
     react(),
     tailwindcss(),
